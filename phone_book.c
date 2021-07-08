67f87c74e5ae2ced9a10b93c9bcb7daf73c219f8
Rahul-d77 <84717074+Rahul-d77@users.noreply.github.com>
2021-07-08 12:51:12 +0530

    phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -64,14 +64,14 @@ int main(int argc, char *argv[]) {
   } else if (strcmp(argv[1], "search") == 0) {  /* Handle search */
      if(argc!=3)
     {
-      print_usage(" Improper arguments for search ", argv[0]);
+      print_usage("Improper arguments for search ", argv[0]);
       exit(1);
     }
     FILE *fp = open_db_file();
     char *name = argv[2];
     if(!search(fp, name)) 
      {
-       printf(" No Match \n");
+       printf("no match\n");
        fclose(fp);
        exit(1);
      }
